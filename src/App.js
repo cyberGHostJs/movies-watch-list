2b567889146fd270905091c0cfd3c6a57bb85e3b
Igweokolo Davidson <davidsonigweokolo@gmail.com>
2023-07-26 22:31:34 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import React, { useState } from "react";
-import Watchlist from "./components/Watchlist";
-import MovieSearch from "./components/MovieSearch";
+import React, { useState, lazy, Suspense } from "react";
 import axios from "axios";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
@@ -9,13 +7,18 @@ import "./styles/movieSearch.css";
 import "./styles/navigation.css";
 import "./styles/watchList.css";
 import PageNotFound from "./components/PageNotFound";
+import LoadingSpinner from "./components/LoadingSpinner"; // Import the LoadingSpinner component
 
 const API_KEY = "a75e655a62afe0300c1b7ad027ce2308";
 const BASE_URL = "https://api.themoviedb.org/3";
 
+// Lazy load the MovieSearch and Watchlist components
+const MovieSearch = lazy(() => import("./components/MovieSearch"));
+const Watchlist = lazy(() => import("./components/Watchlist"));
+
 const App = () => {
   const [searchResults, setSearchResults] = useState([]);
-  const [fetchErr, serFetchErr] = useState(false);
+  const [fetchErr, setFetchErr] = useState(false);
 
   const fetchMovies = async (searchTerm) => {
     try {
@@ -26,12 +29,10 @@ const App = () => {
         },
       });
       setSearchResults(response.data.results);
-      // console.log(response.data.results);
-      serFetchErr(false);
+      setFetchErr(false);
     } catch (error) {
-      //handle err
       console.error("Error fetching movies:", error);
-      serFetchErr(true);
+      setFetchErr(true);
     }
   };
 
@@ -44,20 +45,22 @@ const App = () => {
   };
 
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <MovieSearch
-            watchlist={searchResults}
-            onSearch={handleSearch}
-            fetchErr={fetchErr}
-          />
-        }
-      />
-      <Route path="/watchlist" element={<Watchlist />} />
-      <Route path="*" element={<PageNotFound />} />
-    </Routes>
+    <Suspense fallback={<LoadingSpinner />}>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <MovieSearch
+              watchlist={searchResults}
+              onSearch={handleSearch}
+              fetchErr={fetchErr}
+            />
+          }
+        />
+        <Route path="/watchlist" element={<Watchlist />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </Suspense>
   );
 };
 
---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,10 @@
+
+const LoadingSpinner = () => {
+  return (
+    <div className="loading-spinner-container">
+      <div className="loading-spinner"></div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
